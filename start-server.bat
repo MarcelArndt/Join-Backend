86937ec8e3cc3150d4bb4f65e07a59dd01103858
MarcelArndt <marcel.arndt@hotmail.com>
2025-03-08 17:40:17 +0100

    put gitignore in gitignore

--- start-server.bat
+++ start-server.bat
@@ -1,16 +1,16 @@
 @echo off
 setlocal
 
-REM Überprüfen, ob das Verzeichnis "env" existiert
+
 if not exist env (
     echo Erstelle virtuelle Umgebung "env"...
     python -m venv env
 )
 
-REM Aktivieren der virtuellen Umgebung
+
 call env\Scripts\activate
 
-REM Starte den Django-Server
+
 echo Starte Django-Server...
 python manage.py runserver
 
